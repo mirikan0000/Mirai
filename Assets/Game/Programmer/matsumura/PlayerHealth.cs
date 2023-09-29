35b9940b49f907191bbac234ccd3b82229eeb15f
Mirikan_0527 <hal.takatori0527@gmail.com>
2023-09-29 18:27:13 +0900

    EndScene追加。

--- Assets/Game/Programmer/matsumura/PlayerHealth.cs
+++ Assets/Game/Programmer/matsumura/PlayerHealth.cs
@@ -1,9 +1,16 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHP = 100; // 最大HP
     private int currentHP;  // 現在のHP
+
+    [Header("次シーン遷移時_読み込みシーンリスト")]
+    public List<string> loadSceneNameList;
+    private bool isDestroyScene;
+    private IEnumerable<string> unLoadSceneNameList;
+    private string loadSceneName;
 
     private void Start()
     {
@@ -24,5 +31,26 @@ public class PlayerHealth : MonoBehaviour
         // 死亡時の処理をここに記述（例：ゲームオーバー画面の表示など）
         // この例ではプレイヤーオブジェクトを無効にします。
         gameObject.SetActive(false);
+        SceneManager.Instance.LoadScene("EndScene");
+
+        if (isDestroyScene)
+        {
+
+            // シーン削除
+            foreach (string unLoadSceneName in unLoadSceneNameList)
+            {
+                if (unLoadSceneName != loadSceneName)
+                {
+                    foreach (UnityEngine.SceneManagement.Scene scene in UnityEngine.SceneManagement.SceneManager.GetAllScenes())
+                    {
+                        if (scene.name == unLoadSceneName)
+                        {
+                            SceneManager.Instance.DestroyScene(unLoadSceneName);
+                        }
+                    }
+                }
+            }
+            isDestroyScene = false;
+        }
     }
 }

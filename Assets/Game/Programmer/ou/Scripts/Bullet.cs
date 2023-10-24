fce960e466894152b40a7e5af8a877bf528df0d2
wang-xingchun <415204357@qq.com>
2023-10-24 14:28:42 +0900

    弾丸反射
    変更したのがBullet.csです。

--- Assets/Game/Programmer/ou/Scripts/Bullet.cs
+++ Assets/Game/Programmer/ou/Scripts/Bullet.cs
@@ -4,31 +4,75 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    float RangeOffset = 0.0f;
-    public float Move_Speed = 15.0f;
+    //”­Ë‹——£‚ğ’²®‚·‚é‚½‚ß
+    float rangeOffset = 0.0f;
+    //’eŠÛ‚ÌˆÚ“®‘¬“x
+    public float move_Speed = 15.0f;
+    //’eŠÛ‚Ì„‘Ì
     Rigidbody rb;
+    //’eŠÛ‚Ì‰æ‘œ
+    public GameObject Image_Bullet;
+
+    //“–‚½‚é‚©‚Ç‚¤‚©
+    private bool IsCollision;
+    //ˆÚ“®•ûŒü
+    private Vector3 direction;
 
     // Start is called before the first frame update
     void Start()
     {
+        //„‘Ì‚ğæ“¾‚·‚é
         rb = GetComponent<Rigidbody>();
 
+        //”­Ë‹——£‚ğæ“¾‚·‚é(’eŠÛ—\‘ªü‚ğŒvZ‚·‚é‚É•Û‘¶‚·‚é)
         if(PlayerPrefs.HasKey("Bullet_RangeOffset"))
-            RangeOffset = PlayerPrefs.GetFloat("Bullet_RangeOffset");
+            rangeOffset = PlayerPrefs.GetFloat("Bullet_RangeOffset");
 
-        PlayerPrefs.SetFloat("Bullet_Speed", Move_Speed);
+        //’eŠÛ‘¬“x‚ğ•Û‘¶‚·‚é
+        PlayerPrefs.SetFloat("Bullet_Speed", move_Speed);
 
+        //3•bŒã‚Å©•ª‚ğ”j‰ó‚·‚é
         Destroy(this.gameObject, 3f);
+
+        //’eŠÛ‚Ì‰æ‘œ‚ğì¬‚·‚é
+        GameObject image = Instantiate(Image_Bullet, transform.position, Quaternion.identity);
+        //eqŠÖŒW‚ğİ’è‚·‚é
+        image.transform.parent = this.transform;
+
+        //ˆÚ“®•ûŒü‚ğ‰Šú‰»‚·‚é
+        IsCollision = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (RangeOffset != 0.0f)
+        //”­Ë‹——£‚ğ‡‚í‚¹‚éˆ×‚É©•ª‚ğ—Í‚ğ‚ ‚°‚é
+        if (rangeOffset != 0.0f)
         {
-            rb.AddForce(new Vector3(0, RangeOffset * Time.deltaTime, 0));
+            rb.AddForce(new Vector3(0, rangeOffset * Time.deltaTime, 0));
         }
 
-        transform.Translate(new Vector3(0, 0, Move_Speed * Time.deltaTime));
+        //’eŠÛ”­Ë‚ÌˆÚ“®
+        //•Ç‚Æ‚ ‚Á‚½‚çˆÚ“®•ûŒü‚ÖˆÚ“®‚·‚é
+        if (IsCollision)
+        {
+            //transform.Translate(new Vector3(direction));
+
+            transform.position += direction * move_Speed * Time.deltaTime;
+            return;
+        }
+        transform.Translate(new Vector3(0, 0, move_Speed * Time.deltaTime));
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //•Ç‚Æ‚ ‚Á‚½‚çˆÚ“®•ûŒü‚ğİ’è‚·‚é
+        if (collision.gameObject.tag.Equals("Wall"))
+        {
+            Vector3 dir = Vector3.Reflect(transform.forward, collision.GetContact(0).normal);
+            direction = dir;
+
+            IsCollision = true;
+        }
     }
 }

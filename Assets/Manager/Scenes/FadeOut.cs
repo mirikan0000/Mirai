a77da7d5a21ca641bb73e5a4f7abb3b403841e86
takatori <123847690+mirikan0000@users.noreply.github.com>
2023-11-06 13:12:17 +0900

    ゲームシーンにフェードイン追加

--- Assets/Manager/Scenes/FadeOut.cs
+++ Assets/Manager/Scenes/FadeOut.cs
@@ -13,22 +13,29 @@ public class FadeOut : MonoBehaviour
     Image fadealpha;               //フェードパネルのイメージ取得変数
 
     private float alpha;           //パネルのalpha値取得変数
-    [SerializeField, Range(0.0f, 1.0f)]
+    [SerializeField, Range(0.0f, 0.01f)]
     public float alphaSpeed;
      public bool fadeout;          //フェードアウトのフラグ変数
-
+     public bool fadein;
 
     // Use this for initialization
     void Start()
     {
+        
+    }
+    private void Update()
+    {
+        if (fadein)
+        {
+            FadeIn();
+        }
+    }
+
+    public void Fadeout()
+    {
         fadealpha = Panelfade.GetComponent<Image>(); //パネルのイメージ取得
         alpha = fadealpha.color.a;                 //パネルのalpha値を取得
         fadeout = true;                             //シーン読み込み時にフェードインさせる
-    }
-
-  
-    public void Fadeout()
-    {
         alpha += alphaSpeed;
         fadealpha.color = new Color(0, 0, 0, alpha);
         if (alpha >= 1)
@@ -36,6 +43,20 @@ public class FadeOut : MonoBehaviour
             fadeout = false;
         }
     }
-
+    public void FadeIn()
+    {
+        fadealpha = Panelfade.GetComponent<Image>(); //パネルのイメージ取得
+        alpha = fadealpha.color.a;                 //パネルのalpha値を取得
+        fadeout = true;                             //シーン読み込み時にフェードインさせる
+   
+            alpha -= alphaSpeed;
+            fadealpha.color = new Color(0, 0, 0, alpha);
+        
+       
+        if (alpha <= 0)
+        {
+            fadein = false;
+        }
+    }
 
 }
